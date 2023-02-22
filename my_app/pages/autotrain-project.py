b0ebc5ae2067c00b5c4fcca43ef9274d18ee1bf8
david <david.m.berenstein@gmail.com>
2023-02-23 00:19:28 +0100

    improved logic for getting started with autotrain

--- my_app/pages/autotrain-project.py
+++ my_app/pages/autotrain-project.py
@@ -6,7 +6,7 @@ import streamlit_analytics
 from huggingface_hub import DatasetFilter, HfApi, ModelFilter
 from streamlit_datalist import stDatalist
 from utils.autotrain import get_projects, schedule_retrain, task_id_mapping
-from utils.commons import login_workflow
+from utils.commons import argilla_login_flow, get_dataset_list, hf_login_flow
 
 st.set_page_config(
     page_title="Argilla - Autotrain Project",
@@ -15,25 +15,18 @@ st.set_page_config(
 )
 
 streamlit_analytics.start_tracking(load_from_json=f"{__file__}.json")
-st.image("https://docs.argilla.io/en/latest/_static/images/logo-light-mode.svg")
-st.title("Autotrain Project")
 
-# login workflow
-api_url = login_workflow()
+api_url, api_key = argilla_login_flow("Autotrain Project")
 
-hf_auth_token = os.environ.get("HF_AUTH_TOKEN", "")
-if not hf_auth_token:
-    hf_auth_token = st.sidebar.text_input(
-        "HuggingFace [User Access Tokens](https://huggingface.co/settings/tokens)",
-        os.environ.get("HF_AUTH_TOKEN", ""),
-    )
-if not hf_auth_token:
-    st.error(
-        "Please provide a HuggingFace [User Access"
-        " Tokens](https://huggingface.co/settings/tokens) or set `HF_AUTH_TOKEN` as"
-        " environment variable"
-    )
-    st.stop()
+st.write(
+    """
+    This page allows you to share your dataset from Argilla to HuggingFace Hub without requiring any code!
+    In the background it uses `argilla.load().prepare_for_training()` and `datasets.push_to_hub()`.
+    """
+)
+
+hf_auth_token, api = hf_login_flow()
+
 api = HfApi(token=hf_auth_token)
 user_info = api.whoami()
 namespaces = [user_info["name"]] + [org["name"] for org in user_info["orgs"]]
@@ -41,10 +34,12 @@ namespaces = [user_info["name"]] + [org["name"] for org in user_info["orgs"]]
 projects = get_projects(hf_auth_token)
 project_ids = [proj["proj_name"] for proj in projects]
 
-autotrain_project_name = stDatalist(
-    "Select an existing AutoTrain project (press arrow down) or create a new one",
-    project_ids,
+autotrain_project_name = st.selectbox(
+    "Autotrain project name", options=project_ids + ["other"]
 )
+if autotrain_project_name == "other":
+    autotrain_project_name = st.text_input("Autotrain project name")
+
 project_id = None
 for proj in projects:
     if proj["proj_name"] == autotrain_project_name:
@@ -52,29 +47,21 @@ for proj in projects:
         break
 
 target_namespace = st.selectbox(
-    "Target namespace for saving trained model",
+    "Target HuggingFace namespace for saving trained model",
     options=namespaces,
     help="the namespace where the trained model should end up",
 )
 
-input_dataset = st.text_input(
-    "Input Dataset from the hub [from the hub](https://huggingface.co/models) or"
-    f" [Argilla]({api_url})",
-    "davidberenstein1957/ray-summit-classy",
-    help="the dataset on which the model will be trained",
+datasets_list = [
+    f"{ds['owner']}/{ds['name']}" for ds in get_dataset_list(api_url, api_key)
+]
+dataset_argilla = st.selectbox(
+    "Argilla or Hub Dataset Name", options=datasets_list + ["Custom HuggingFace Hub"]
 )
-try:
-    ds = rg.load("input_dataset")
-    ds_ds = ds.prepare_for_training(framework="transformers", train_size=0.8)
-    ds_ds.push_to_hub(input_dataset, token=hf_auth_token)
-except:
-    potential_datasets = api.list_datasets(
-        filter=DatasetFilter(dataset_name=input_dataset)
-    )
-    if not len(potential_datasets) == 1:
-        st.warning("Please select a dataset from the list below:")
-        st.write(potential_datasets)
-        st.stop()
+dataset_argilla_name = dataset_argilla.split("/")[-1]
+dataset_argilla_workspace = dataset_argilla.split("/")[0]
+
+
 input_model = st.text_input(
     "Input Model [from the hub](https://huggingface.co/models)",
     value="bert-base-uncased",
@@ -89,9 +76,28 @@ if not len(potential_models) == 1:
 
 task = st.selectbox("Task", task_id_mapping)
 task_id = task_id_mapping[task]
-
 start = st.button("Schedule Autotrain")
 if start:
+    if dataset_argilla != "Custom HuggingFace Hub":
+        rg.set_workspace(dataset_argilla_workspace)
+        ds = rg.load(dataset_argilla_name)
+        ds_ds = ds.prepare_for_training(framework="transformers", train_size=0.8)
+        ds_ds.push_to_hub(
+            f"{target_namespace}/{dataset_argilla_name}",
+            token=hf_auth_token,
+            private=True,
+        )
+    else:
+        input_dataset = st.text_input(
+            "Input Dataset from the hub [from the hub](https://huggingface.co/datasets)"
+        )
+        potential_datasets = api.list_datasets(
+            filter=DatasetFilter(dataset_name=input_dataset)
+        )
+        if not len(potential_datasets) == 1:
+            st.warning("Please select a correct dataset from the list below:")
+            st.write(potential_datasets)
+            st.stop()
     schedule_retrain(
         hf_auth_token=hf_auth_token,
         target_namespace=target_namespace,
